bc121a6e2cf25506e1cee410865dd6e7f1dd860e
Carlos-Bolano <carlostutos828@gmail.com>
2025-01-18 14:10:06 -0500

    initial commit

---
+++ constans.ts
@@ -0,0 +1,79 @@
+export const navItems = [
+  {
+    title: "Experiencia",
+    label: "experiencia",
+    url: "/#experiencia",
+  },
+  {
+    title: "Proyectos",
+    label: "proyectos",
+    url: "/#proyectos",
+  },
+  {
+    title: "Sobre mí",
+    label: "sobre-mi",
+    url: "/#sobre-mi",
+  },
+  {
+    title: "Contacto",
+    label: "contacto",
+    url: "mailto:carlostutos828@gmail.com",
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Moodflix",
+    description:
+      "An AI-powered movie recommendation app that analyzes users' emotional state from their text input and suggests movies that match their mood.",
+    link: "https://moodflix-by-calisto.vercel.app",
+    image: "/projects/adventjs.webp",
+    github: "https://github.com/Carlos-Bolano/moodflix",
+    // tags: [TAGS.NEXT, TAGS.REACT, TAGS.TAILWIND, TAGS.ESLINT, TAGS.GEMINI, TAGS.IA_SDK],
+  },
+  {
+    title: "Jadirh Gonzalez Poems",
+    description:
+      "A poetry blog I designed and developed for a client passionate about writing. The site provides an interactive experience where fans can explore, comment, and like Jadirh González's poems.",
+    link: "https://jadirhgonzalezpoems.vercel.app",
+    image: "/projects/project-06.webp",
+    github: "https://github.com/Carlos-Bolano/jadirhgonzalezpoems",
+    // tags: [TAGS.NEXT, TAGS.SHADCN, TAGS.TYPESCRIPT, TAGS.NEXTAUTH, TAGS.MONGODB],
+  },
+  {
+    title: "Age Calculator",
+    description:
+      "A solution to a challenge from Frontend Mentor, designed to improve development skills. This project focused on designing and implementing a user interface and calculation logic for an age calculator.",
+    link: "https://age--calculator.vercel.app/",
+    image: "/projects/project-04.webp",
+    github: "https://github.com/Carlos-Bolano/age-calculator",
+    // tags: [TAGS.REACT, TAGS.TYPESCRIPT, TAGS.TAILWIND, TAGS.VITE, TAGS.ESLINT],
+  },
+  {
+    title: "Task Manager",
+    description:
+      "A task management app built on the MERN stack (MongoDB, Express, React, Node.js). It features robust user authentication and CRUD functionality, allowing users to manage and filter tasks by categories. ",
+    link: "https://manejador-de-tareas.vercel.app",
+    image: "/projects/project-02.webp",
+    github: "https://github.com/Carlos-Bolano/Task-Organizer",
+    // tags: [TAGS.REACT, TAGS.NODE, TAGS.EXPRESS, TAGS.MONGODB, TAGS.TAILWIND],
+  },
+  {
+    title: "Travlog Landing Page",
+    description:
+      "Built based on a Figma design from the community, initially designed for desktop. I adapted it for mobile and developed it with AstroJS, learning new skills while creating it.",
+    link: "https://travlog-website.vercel.app/",
+    image: "/projects/travlog.webp",
+    github: "https://github.com/Carlos-Bolano/Travlog-Landing-Page",
+    // tags: [TAGS.HTML, TAGS.CSS, TAGS.JAVASCRIPT, TAGS.ASTRO, TAGS.TAILWIND],
+  },
+  {
+    title: "Believe in YourSelf",
+    description:
+      "A responsive GYM website showcasing my focus on user experience. The design works seamlessly from desktop to mobile devices.",
+    link: "https://believe-in-yourself.netlify.app/",
+    image: "/projects/believe-in-yourself.webp",
+    github: "https://github.com/Carlos-Bolano/Believe_in_YourSelf",
+    // tags: [TAGS.HTML, TAGS.CSS, TAGS.JAVASCRIPT],
+  },
+];
